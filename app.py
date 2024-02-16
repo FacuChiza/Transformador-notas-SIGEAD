de5844f736913ecb27b3a4560274f969cd6e6659
facuchiza <facuchiarenza@gmail.com>
2024-02-16 14:42:55 -0300

    cambios en estructura y redondeo de las notas

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ import pandas as pd
 
 app = Flask(__name__)
 
+def redondear(numero, decimales=0):
+  if isinstance(numero, str) and numero == "-":
+    return "Ausente" 
+  try:
+    numero_entero = int(numero)
+    if numero - numero_entero >= 0.5:
+      return numero_entero + 1
+    else:
+      return numero_entero
+  except ValueError:
+    return numero
+
 @app.route('/')
 def index():
   return render_template('success.html')
@@ -18,16 +30,13 @@ def upload_file():
     filename = f"Notas Transformadas {materia}.csv"
     
     os.makedirs("uploads", exist_ok=True)
-    
     file.save(os.path.join("uploads", filename))
-    
-    
     df = pd.read_excel(os.path.join("uploads", filename))
     df.columns = ["Nom", "Ape", "Num", "Inst", "Depa", "Cues", "Ult"]
     df = df.drop(columns=["Nom", "Ape", "Inst", "Ult"])
     df.columns = ["Personal", "CursadaId", "Nota"]
-    df.iloc[0:, 1] = codigo_materia
-    df.replace("-", "Ausente", inplace=True)
+    df["Nota"] = df["Nota"].apply(redondear, decimales=2)
+    df.iloc[0:, 1] = codigo_materia    
     df.to_csv(os.path.join("uploads", filename), index=False)
 
     return redirect(url_for("download_file", filename=filename))
